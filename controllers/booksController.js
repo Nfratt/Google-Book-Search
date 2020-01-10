14157c7ead6a35a96e6aeb1635d12580d397e34d
Nick Frattaroli <Nafratt@gmail.com>
2020-01-10 16:21:14 -0500

    adding find by id,create,update,remove

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -6,7 +6,35 @@ module.exports= {
         .find(req.query)
         .sort({date:-1})
         .then(dbModel=>res.json(dbModel))
-        .catch(err=>res.status(422).json(err));
+        .catch(err=>res.status(404).json(err));
     },
+    findbyid:function(req,res){
+        db.book
+        .find(req.query)
+        .sort({date:-1})
+        .then(dbModel=>res.json(dbModel))
+        .catch(err=>res.status(404).json(err));
+    },
+    create: function(req, res) {
+        console.log("Made it here")
+        db.Book
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(404).json(err));
+      },
+      update: function(req, res) {
+        db.Book
+          .findOneAndUpdate({ _id: req.params.id }, req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(404).json(err));
+      },
+      remove: function(req, res) {
+        db.Book
+          .findById({ _id: req.params.id })
+          .then(dbModel => dbModel.remove())
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(404).json(err));
+      }
+    };
     
-}
+
